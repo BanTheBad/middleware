759c5ddc5a3878cfba91096b798ad4e018d1d90b
Uche Ozoemena <ucheozoemena@gmail.com>
2020-10-21 00:41:42 +0100

    Create Google Oauth strategy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const passport = require('passport');
 const { Strategy: LocalStrategy } = require('passport-local');
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
 const apiDoc = require('../docs/ban-the-bad-docs.json');
 const controllers = require('./controllers');
 const errorHandler = require('./middleware/errorHandler');
@@ -19,6 +20,19 @@ passport.use(new LocalStrategy((username, password, done) => {
   // TODO: authenticate based on the database and user model
   done(null, true);
 }));
+
+// Google Oauth strategy
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `http://localhost:${process.env.PORT}/auth/google/callback`,
+  },
+  (accessToken, refreshToken, profile, done) => {
+    // TODO: check tokens as necessary
+    return done(null, profile);
+  }
+));
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiDoc));
 
