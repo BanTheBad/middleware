fcd1115a6aaa099e7c2a49ad1bb3c98578d18f58
Uche Ozoemena <ucheozoemena@gmail.com>
2020-10-21 01:38:43 +0100

    Deactivate the local strategy login for now

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,11 +1,22 @@
 const passport = require('passport');
+const express = require('express');
+const app = express();
 
-module.exports = function(req, res) {
-    passport.authenticate('local', (err, user) => {
+app.get('/', (req, res) => {
+    console.log('inside login middleware');
+    return res.status(200).json({
+        status: 'success',
+        message: 'login success',
+    });
+    /*
+    passport.authenticate('local', { failureRedirect: '/' }, (err, user) => {
         // TODO: verify a user based on the database and user models
         return res.status(200).json({
             status: 'success',
             message: 'login success',
         });
     });
-};
+    */
+});
+
+module.exports = app;
