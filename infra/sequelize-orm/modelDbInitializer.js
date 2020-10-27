a84ec6647f5f9acee6609aa3cae3ec1f2a611e55
Ekundayo Abiona <ekundayoabiona@gmail.com>
2020-10-27 07:56:52 +0100

    ch(folder-structure): Hardcode model path names

--- infra/sequelize-orm/modelDbInitializer.js
+++ infra/sequelize-orm/modelDbInitializer.js
@@ -15,32 +15,46 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config
+    config,
   );
 }
 
 const sourceFolder = path.resolve('src');
 
-fs.readdirSync(path.resolve('src'))
-  .filter((folderName) => folderName !== 'app.js' && folderName !== 'server.js')
-  .forEach((moduleFolder) => {
-    fs.readdirSync(path.join(`${sourceFolder}/${moduleFolder}`))
-      .filter((file) => {
-        return (
-          file.indexOf('.') !== 0 &&
-          file !== basename &&
-          file !== 'app' &&
-          file.slice(-8) === 'Model.js'
-        );
-      })
-      .forEach((file) => {
-        const model = require(path.join(sourceFolder, '/', moduleFolder, file))(
-          sequelize,
-          Sequelize.DataTypes
-        );
-        db[model.name] = model;
-      });
-  });
+/*
+ * Add folder containing a someNameModel.js file here
+ *
+ * Another way is to maybe pass in folder list
+ * from outside where the modelDBInitializer is being called so
+ * it can be seen outside this infrastructure
+ *
+ * Also, this can be done dynamically and filtered as needed instead of
+ * hardcoding the folder names.
+ *
+ * ```.js
+ * fs.readdirSync(path.resolve('src'))
+ * .filter((folderName) => folderName !== 'app.js' && folderName !== 'server.js')
+ * ```
+ */
+const modelsPath = ['accused', 'auth', 'cases', 'users', 'victims'];
+
+modelsPath.forEach((moduleFolder) => {
+  fs.readdirSync(path.join(`${sourceFolder}/${moduleFolder}`))
+    .filter((file) => {
+      return (
+        file.indexOf('.') !== 0
+        && file !== basename
+        && file.slice(-8) === 'Model.js'
+      );
+    })
+    .forEach((file) => {
+      const model = require(path.join(sourceFolder, '/', moduleFolder, file))(
+        sequelize,
+        Sequelize.DataTypes,
+      );
+      db[model.name] = model;
+    });
+});
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
